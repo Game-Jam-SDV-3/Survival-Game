070c787c9a80d9e146ddab4a7d6515c7659cdf78
Titinite <thibault.leray@supdevinci-edu.fr>
2025-02-18 11:47:56 +0100

    fix health bar text

--- Assets/Scripts/Interface/HealthBarUI.cs
+++ Assets/Scripts/Interface/HealthBarUI.cs
@@ -1,24 +1,29 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
     public Image healthFill;
+    public TMP_Text healthText;
 
     public Player player;
 
     void Start()
     {
         healthFill.fillAmount = player.health / player.maxHealth;
+        healthText.text = player.health.ToString();
     }
 
     void Update()
     {
         healthFill.fillAmount = (float)player.health / player.maxHealth;
+        healthText.text = player.health.ToString();
     }
 
     public void SetMaxHealth(int maxHealth)
     {
         healthFill.fillAmount = 1;
+        player.maxHealth = maxHealth;
     }
 }
